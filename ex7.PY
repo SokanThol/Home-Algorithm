d02a9df9cac14d7f9a6def569704bd6ac3a74dee
unknown <SokAn.Thol@PNC.LAN>
2024-08-04 10:48:27 +0700

    Hi

---
+++ ex7.PY
@@ -0,0 +1,7 @@
+value = input("number")
+if value >= 5 :
+    print("Apple")
+elif value <= 10 :
+    print("Banana")
+# Q1 : To see print "Apple"
+# Q2 : problem/instruction this code is solving (« Apple ») -> (" ")
